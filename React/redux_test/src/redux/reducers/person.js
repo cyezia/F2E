cea3907e417b39cd1df9dbe1f894a94315530160
cyezia <634940800@qq.com>
2022-05-28 15:22:13 +0800

    update 48

--- React/redux_test/src/redux/reducers/person.js
+++ React/redux_test/src/redux/reducers/person.js
@@ -2,7 +2,7 @@ import { ADD_PERSON } from "../constant";
 
 // 初始化人的列表
 const initState = [{id: '001', name: 'Tom', age: 18}];
-export default function countReducer(preState=initState, action) {
+export default function personReducer(preState=initState, action) {
   // console.log('personReducer执行了');
   const {type, data} = action;
   switch(type) {
